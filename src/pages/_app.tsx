27d954c7f2cb8215d1c7c061835c0d81adf00ac0
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-28 12:35:39 +0900

    style : add theme style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { theme } from "@/styles/theme";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  logoColor: "#2A54E5",
+  Black: "#1F3454",
+  Placeholder: "#798195",
+  Normal: "#394050",
+  Blue: "#1556F7",
+  Gray100: "#C3C9D9",
+  Gray300: "#7E82A6",
+  backgroundColor100: "#F5F8FE",
+  backgroundColor300: "#EDF0FE",
+};
