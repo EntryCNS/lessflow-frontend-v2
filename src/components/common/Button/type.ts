eca66f92db6e584267aa1cd25e9a8e9b3a83003b
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-28 13:08:21 +0900

    fix : Button Type

--- src/components/common/Button/type.ts
+++ src/components/common/Button/type.ts
@@ -1,10 +1,10 @@
 import React, { ButtonHTMLAttributes, ReactElement } from "react";
 
 export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  onClickHandler?: React.MouseEventHandler<HTMLButtonElement>;
-  disable?: boolean;
+  onClickHandler: React.MouseEventHandler<HTMLButtonElement>;
+  disable: boolean;
+  active: boolean | null;
   children?: ReactElement | string;
   width?: string | null;
   height?: string | null;
-  active?: boolean | null;
 }
