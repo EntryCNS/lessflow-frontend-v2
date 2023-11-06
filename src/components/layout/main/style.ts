52280a603259f3642fa141a62b4f16c5611f5db7
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-06 13:43:17 +0900

    add : EntirePage

--- src/components/layout/main/style.ts
+++ src/components/layout/main/style.ts
@@ -3,28 +3,28 @@ import styled from "styled-components";
 import { theme } from "@/styles/theme";
 
 export const MainLayout = styled.div`
-    height: 100%;
-    min-height: calc(100vh - 73px);
+  height: 100%;
+  min-height: calc(100vh - 73px);
 
-    background-color: ${theme.backgroundColor100};
-`
+  background-color: ${theme.backgroundColor100};
+`;
 
 export const HeaderPlace = styled.div`
-    // width: 100vw;
-    height: 73px;
-`
+  // width: 100vw;
+  height: 73px;
+`;
 
 export const MainBannerBox = styled.div`
-    padding: 0 12.8vw;
+  padding: 0 12.8vw;
 
-    width: 100%;
-    height: 25.6vw;
-    background-color: #FFFFFF;
+  width: 100%;
+  height: 25.6vw;
+  background-color: #ffffff;
 
-    display: flex;
-    justify-content: space-between;
-    align-items: end;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+`;
 
 /* export const MainBannerSmallText = styled.div`
     font-family: 'NanumSquareRound';
@@ -33,71 +33,69 @@ export const MainBannerBox = styled.div`
 ` */
 
 export const MainTxtImg = styled(Image)`
-    margin-bottom: 5vw;
+  margin-bottom: 5vw;
 
-    width: 34vw;
-    overflow-y: visible;
-    object-fit: cover;
-`
+  width: 34vw;
+  overflow-y: visible;
+  object-fit: cover;
+`;
 
 export const MainCharacterImg = styled(Image)`
-    width: 26.4vw;
-    height: 23.2vw;
-    object-fit: cover;
-`
+  width: 26.4vw;
+  height: 23.2vw;
+  object-fit: cover;
+`;
 
 export const MainContentBox = styled.div`
-    padding: 3vw 3vw 0 3vw;
-    width: 100%;
-`
+  padding: 3vw 3vw 0 3vw;
+  width: 100%;
+`;
 
 export const NewsWrapper = styled.div`
-    padding: 1.9vw 0;
+  padding: 1.9vw 0;
 
-    width: 100%;
+  width: 100%;
 
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 
 export const NewsBox = styled.div`
-    // margin: 0 2.9vw;
+  // margin: 0 2.9vw;
 
-    width: 16.3vw;
-    height: 23.2vw;
+  width: 16.3vw;
+  height: 23.2vw;
 
-    position: relative;
-`
+  position: relative;
+`;
 
 export const NewsBoxImg = styled(Image)`
-    width: inherit;
-    height: inherit;
-    border-radius: 15px;
+  width: inherit;
+  height: inherit;
+  border-radius: 15px;
 
-    position: absolute;
-`
+  position: absolute;
+`;
 
 export const NewsText = styled.div`
-    width: 100%;
-    height: 4.3vw;
+  width: 100%;
+  height: 4.3vw;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    background-color: #394050;
-    border-radius: 0 0 15px 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    font-size: 1.2vw;
-    color: #FFF;
-    font-family: Pretendard;
-    font-weight: 700;
-    letter-spacing: -0.025rem;
+  background-color: #394050;
+  border-radius: 0 0 15px 15px;
 
-    position: absolute;
-    bottom: 0;
-    z-index: 1;
-`
+  font-size: 1.2vw;
+  color: #fff;
+  font-family: Pretendard;
+  font-weight: 700;
+  letter-spacing: -0.025rem;
 
-
+  position: absolute;
+  bottom: 0;
+  z-index: 1;
+`;
---
+++ src/pages/entire.tsx
@@ -0,0 +1,8 @@
+import EntriePage from "@/components/layout/EntirePage/EntriePage";
+import React from "react";
+
+const entrie = () => {
+  return <EntriePage />;
+};
+
+export default entrie;
