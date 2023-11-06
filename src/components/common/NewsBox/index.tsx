c6d6655b5362956eacb094a2866f1e4a68240a2c
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-06 23:55:36 +0900

    fix : Boxsize

--- src/components/common/NewsBox/index.tsx
+++ src/components/common/NewsBox/index.tsx
@@ -7,7 +7,7 @@ const NewsBox = ({ title, image }: INewsBoxProps) => {
   return (
     <S.InlineBox>
       <S.MainContainer>
-        <Image src={image} alt="" width={450} height={450} />
+        <Image src={image} alt="" width={400} height={400} />
         <S.TitleBox>{title}</S.TitleBox>
       </S.MainContainer>
     </S.InlineBox>
