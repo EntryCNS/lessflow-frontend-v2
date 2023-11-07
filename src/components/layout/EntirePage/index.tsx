49ab5f5a38160475f8cf800a9771165305fe11d3
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-07 11:20:21 +0900

    Add : Server Connect

--- src/components/layout/EntirePage/index.tsx
+++ src/components/layout/EntirePage/index.tsx
@@ -1,75 +1,52 @@
 import Button from "@/components/common/Button";
 import NewsBox from "@/components/common/NewsBox";
 import DefaultTemplate from "@/components/common/Template/DefaultTemplate";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
 import { getArticleList } from "@/util/apis/EntireApis";
-
-const testObj = [
-  {
-    title: "홍준표",
-    image:
-      "https://www.kyongbuk.co.kr/news/photo/202311/2146379_594092_5440.jpg",
-  },
-  {
-    title: "포항 축구",
-    image:
-      "https://www.kyongbuk.co.kr/news/photo/202311/2146379_594092_5440.jpg",
-  },
-  {
-    title: "홍준표1",
-    image:
-      "https://www.kyongbuk.co.kr/news/photo/202311/2146379_594092_5440.jpg",
-  },
-  {
-    title: "포항 축구1",
-    image:
-      "https://www.kyongbuk.co.kr/news/photo/202311/2146379_594092_5440.jpg",
-  },
-  {
-    title: "홍준표2",
-    image:
-      "https://www.kyongbuk.co.kr/news/photo/202311/2146379_594092_5440.jpg",
-  },
-];
+import { INewsBoxProps } from "@/components/common/NewsBox/type";
 
 const EntirePage = () => {
+  const [newsList, setNewsList] = useState<INewsBoxProps[]>([]);
+
   const onclickHandler = () => {
     getArticleList()
-      .then((e) => console.log(e))
+      .then((value) => setNewsList([...value.data]))
       .catch((e) => console.log(e));
   };
+
+  useEffect(() => {
+    onclickHandler();
+  }, []);
 
   return (
     <DefaultTemplate>
       <S.MainContainer>
-        {new Array(Math.ceil(testObj.length / 5)).fill(0).map((_, idx1) => (
+        {new Array(Math.ceil(newsList.length / 5)).fill(0).map((_, idx1) => (
           <S.NewsContentsWrap>
-            <Button disable={false} active onClick={onclickHandler}>
+            <Button disable={false} active onClick={() => {}}>
               안녕하세요
             </Button>
             <S.NewsBoxWrap>
-              {new Array(5).fill(0).map((_, idx2) => {
-                console.log(idx1 * 5 + idx2, testObj[idx1 * 5 + idx2]);
-                return (
-                  <div
-                    key={idx2}
-                    onClick={() =>
-                      console.log(
-                        testObj[idx1 * 5 + idx2].title,
-                        testObj[idx1 * 5 + idx2].image
-                      )
-                    }
-                  >
-                    {testObj[idx1 * 5 + idx2] && (
-                      <NewsBox
-                        title={testObj[idx1 * 5 + idx2].title}
-                        image={testObj[idx1 * 5 + idx2].image}
-                      />
-                    )}
-                  </div>
-                );
-              })}
+              {new Array(5).fill(0).map((_, idx2) => (
+                <div
+                  key={idx2}
+                  onClick={() =>
+                    console.log(
+                      newsList[idx1 * 5 + idx2].keyword,
+                      newsList[idx1 * 5 + idx2].thumbnail,
+                      newsList[idx1 * 5 + idx2].id
+                    )
+                  }
+                >
+                  {newsList[idx1 * 5 + idx2] && (
+                    <NewsBox
+                      keyword={newsList[idx1 * 5 + idx2].keyword}
+                      id={newsList[idx1 * 5 + idx2].id}
+                    />
+                  )}
+                </div>
+              ))}
             </S.NewsBoxWrap>
           </S.NewsContentsWrap>
         ))}
@@ -79,3 +56,5 @@ const EntirePage = () => {
 };
 
 export default EntirePage;
+
+//thumbnail={newsList[idx1 * 5 + idx2].thumbnail}
