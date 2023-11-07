670da9abdd42281c5b5f6fe1f2f943924abd4159
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-07 23:34:08 +0900

    Add : Modal & Stop Scroll

--- src/components/layout/EntirePage/index.tsx
+++ src/components/layout/EntirePage/index.tsx
@@ -5,13 +5,19 @@ import React, { useEffect, useState } from "react";
 import * as S from "./style";
 import { getArticleList } from "@/util/apis/EntireApis";
 import { INewsBoxProps } from "@/components/common/NewsBox/type";
+import NewsModal from "@/components/common/NewsModal";
 
 const EntirePage = () => {
   const [newsList, setNewsList] = useState<INewsBoxProps[]>([]);
+  const [modalActive, setModalActive] = useState<boolean>(false);
+  const [modalId, setModalId] = useState<string>("");
 
   const onclickHandler = () => {
     getArticleList()
-      .then((value) => setNewsList([...value.data]))
+      .then((value) => {
+        console.log(value);
+        setNewsList([...value.data]);
+      })
       .catch((e) => console.log(e));
   };
 
@@ -19,42 +25,70 @@ const EntirePage = () => {
     onclickHandler();
   }, []);
 
+  useEffect(() => {
+    if (modalActive) {
+      document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    }
+    console.log(window.scrollY);
+
+    return () => {
+      if (modalActive) {
+        const scrollY = document.body.style.top;
+        document.body.style.cssText = "";
+        window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+      }
+    };
+  }, [modalActive]);
+
   return (
     <DefaultTemplate>
-      <S.MainContainer>
-        {new Array(Math.ceil(newsList.length / 5)).fill(0).map((_, idx1) => (
-          <S.NewsContentsWrap>
-            <Button disable={false} active onClick={() => {}}>
-              안녕하세요
-            </Button>
-            <S.NewsBoxWrap>
-              {new Array(5).fill(0).map((_, idx2) => (
-                <div
-                  key={idx2}
-                  onClick={() =>
-                    console.log(
-                      newsList[idx1 * 5 + idx2].keyword,
-                      newsList[idx1 * 5 + idx2].thumbnail,
-                      newsList[idx1 * 5 + idx2].id
-                    )
-                  }
-                >
-                  {newsList[idx1 * 5 + idx2] && (
-                    <NewsBox
-                      keyword={newsList[idx1 * 5 + idx2].keyword}
-                      id={newsList[idx1 * 5 + idx2].id}
-                    />
-                  )}
-                </div>
-              ))}
-            </S.NewsBoxWrap>
-          </S.NewsContentsWrap>
-        ))}
-      </S.MainContainer>
+      <>
+        {modalActive && (
+          <NewsModal id={modalId} onClick={() => setModalActive(false)} />
+        )}
+        <S.MainContainer>
+          {new Array(Math.ceil(newsList.length / 5)).fill(0).map((_, idx1) => (
+            <S.NewsContentsWrap>
+              <Button disable={false} active onClick={() => {}}>
+                안녕하세요
+              </Button>
+              <S.NewsBoxWrap>
+                {new Array(5).fill(0).map((_, idx2) => (
+                  <div
+                    key={idx2}
+                    onClick={() =>
+                      console.log(
+                        newsList[idx1 * 5 + idx2].keyword,
+                        newsList[idx1 * 5 + idx2].thumbnail
+                      )
+                    }
+                  >
+                    {newsList[idx1 * 5 + idx2] && (
+                      <NewsBox
+                        keyword={newsList[idx1 * 5 + idx2].keyword}
+                        thumbnail={newsList[idx1 * 5 + idx2].thumbnail}
+                        onClick={() => {
+                          setModalId(newsList[idx1 * 5 + idx2].id);
+                          setModalActive(true);
+                        }}
+                        id={newsList[idx1 * 5 + idx2].id}
+                      />
+                    )}
+                  </div>
+                ))}
+              </S.NewsBoxWrap>
+            </S.NewsContentsWrap>
+          ))}
+        </S.MainContainer>
+      </>
     </DefaultTemplate>
   );
 };
 
 export default EntirePage;
 
-//thumbnail={newsList[idx1 * 5 + idx2].thumbnail}
+//
