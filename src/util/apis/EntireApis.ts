e61392083d6579f5cf233a5b7ab7c3cf211c7b0b
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-07 23:33:06 +0900

    add : get Artivle Info

--- src/util/apis/EntireApis.ts
+++ src/util/apis/EntireApis.ts
@@ -4,3 +4,13 @@ export const getArticleList = () => {
   const response = API.get("/article/");
   return response;
 };
+
+export const getArtileInformation = (id: string) => {
+  const response = API.get(`/article/${id}`);
+  return response;
+};
+
+export const getArtileVideo = (id: string) => {
+  const response = API.get(`/article/video/${id}`);
+  return response;
+};
