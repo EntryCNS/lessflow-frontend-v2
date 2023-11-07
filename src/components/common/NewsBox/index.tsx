1222a7ca32ea5fbf3d661ec79697581bc52db276
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-07 11:19:46 +0900

    fix : NewsBox image

--- src/components/common/NewsBox/index.tsx
+++ src/components/common/NewsBox/index.tsx
@@ -3,12 +3,12 @@ import * as S from "./style";
 import { INewsBoxProps } from "./type";
 import Image from "next/image";
 
-const NewsBox = ({ title, image }: INewsBoxProps) => {
+const NewsBox = ({ keyword, thumbnail, id }: INewsBoxProps) => {
   return (
     <S.InlineBox>
       <S.MainContainer>
-        <Image src={image} alt="" width={400} height={400} />
-        <S.TitleBox>{title}</S.TitleBox>
+        {thumbnail && <Image src={thumbnail} alt="" width={400} height={400} />}
+        <S.TitleBox>{keyword}</S.TitleBox>
       </S.MainContainer>
     </S.InlineBox>
   );
