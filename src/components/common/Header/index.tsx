780780c39409a7f43a97c33a411a76f37690715f
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-08 23:38:27 +0900

    fix : Header Context

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -39,7 +39,7 @@ const Header = () => {
           isActive={pageIdx === 0}
           onClick={() => OnClickHandler({ path: "/" })}
         >
-          인기뉴스보기
+          최신뉴스보기
         </S.ContentsContext>
         <S.ContentsContext
           isActive={pageIdx === 1}
